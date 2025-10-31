d9a13b69cca685c35745f395f4f867976e39199c
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-10-31 11:23:08 -0400

    upm-release@v1.0.0
    - Hello World! :tada: :rocket:

--- README.md
+++ README.md
@@ -1,20 +1,51 @@
-# github-action-template
+# upm-release
 
-A GitHub Actions template repository for TypeScript based Actions.
+A GitHub action to sign and release a upm package.
 
 ## How to use
+
+Once a pull request is merged into the target branch, this action will create a new git tag based on the version specified in the `package.json` file located at the root of the repository.
+
+> [!NOTE]
+> Make sure that the `package.json` file contains a valid semantic version (e.g., `1.0.0`, `2.1.3`, etc.) before pushing to the target branch! If the version is not valid, or the git tag already exists, the action will fail.
+
+Once the tag is created, the package will be signed and the resulting `.tgz` file will be uploaded as a release asset to the corresponding GitHub release.
+
+The action will then generate a ***draft*** GitHub release with the tag name and upload the signed package as a release asset.
 
 ### workflow
 
 ```yaml
-steps:
-  - uses: RageAgainstThePixel/github-action-template@v1
+name: UPM Release
+on:
+  push:
+    branches: [upm]
+jobs:
+  release:
+    permissions:
+      contents: write
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v5
+    - uses: RageAgainstThePixel/upm-release@v1
+      with:
+        username: ${{ secrets.UNITY_USERNAME }}
+        password: ${{ secrets.UNITY_PASSWORD }}
+        organization-id: ${{ secrets.UNITY_ORGANIZATION_ID }}
+        package-json: 'path/to/package.json' # optional, default is searching in the repository root for package.json
+        release-title: 'Optional release title for this release.' # optional, default is generated from package name and version
+        release-notes: 'Optional release notes.' # optional, default is generated from commit message that triggered the action
+        github-token: ${{ secrets.GITHUB_TOKEN }} # optional, default is GITHUB_TOKEN secret
 ```
 
 ### inputs
 
 | name | description | required |
 | ---- | ----------- | -------- |
-| .... | ........... | ........ |
-
-### outputs
+| username | The username for the Unity account. | true |
+| password | The password for the Unity account. | true |
+| organization-id | The organization ID for the Unity account. | true |
+| package-json | Path to the package.json file. Defaults to searching in the repository root for package.json. | false |
+| release-title | The title for the GitHub release. If not provided, it will be generated from the package name and version. | false |
+| release-notes | The release notes for the GitHub release. If not provided, it will be generated from the commit message that triggered the action. | false |
+| github-token | GitHub token to create releases and upload assets. Defaults to the GITHUB_TOKEN secret, but may be required if permissions are not set correctly or the repository is private. | false |
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,242 @@
 import core = require('@actions/core');
-
-const IS_POST = !!core.getState('isPost');
+import { exec } from '@actions/exec';
+import * as fs from 'fs/promises';
+import * as github from '@actions/github';
+import * as glob from '@actions/glob';
+import * as path from 'path';
+import {
+    UnityHub,
+    UnityVersion
+} from '@rage-against-the-pixel/unity-cli';
 
 const main = async () => {
     try {
-        if (!IS_POST) {
-            core.info('Hello World!');
-            core.saveState('isPost', true);
-        } else {
-            core.info('Hello World! (post)');
+        const githubToken = core.getInput('github-token', { required: false }) || process.env.GITHUB_TOKEN || undefined;
+
+        if (!githubToken) {
+            throw new Error('GitHub token is required to create a release. Please ensure your workflow enables permissions for GITHUB_TOKEN or pass a personal access token.');
         }
+
+        const username: string = core.getInput('username', { required: true });
+        const password: string = core.getInput('password', { required: true });
+        const organizationId: string = core.getInput('organization-id', { required: true });
+        let releaseNotes: string = core.getInput('release-notes', { required: false });
+
+        await exec('git', ['config', '--global', 'user.name', 'github-actions[bot]']);
+        await exec('git', ['config', '--global', 'user.email', 'github-actions[bot]@users.noreply.github.com']);
+        await exec('git', ['fetch', '--tags', '--force']);
+        const tags = await getTags();
+
+        let packageName = '';
+        let packageVersion = '';
+        let packageJsonPath = core.getInput('package-json', { required: false });
+
+        if (!packageJsonPath) {
+            const globber = await glob.create('**/package.json');
+            const packageJsonFiles = await globber.glob();
+
+            if (packageJsonFiles.length === 0) {
+                throw new Error('No package.json file found in the working directory or its subdirectories');
+            }
+
+            if (packageJsonFiles.length > 1) {
+                throw new Error('Multiple package.json files found in the working directory or its subdirectories. Please ensure there is only one package.json file.');
+            }
+
+            packageJsonPath = packageJsonFiles[0];
+        }
+
+        if (!packageJsonPath) {
+            throw new Error('package.json path is not specified.');
+        }
+
+        try {
+            const stat = await fs.stat(packageJsonPath);
+
+            if (!stat.isFile()) {
+                throw new Error('package.json is not a file.');
+            }
+
+            await fs.access(packageJsonPath, fs.constants.R_OK);
+        } catch (error) {
+            throw new Error('package.json file not found or is not readable.');
+        }
+
+        var packageJsonContent = await fs.readFile(packageJsonPath, { encoding: 'utf-8' });
+        var packageJson = JSON.parse(packageJsonContent);
+        packageName = packageJson.name;
+        packageVersion = packageJson.version;
+
+        if (tags.has(`v${packageVersion}`) || tags.has(packageVersion)) {
+            throw new Error(`Tag for ${packageName} ${packageVersion} already exists @ ${tags.get(`v${packageVersion}`) || tags.get(packageVersion)}`);
+        }
+
+        const lastTag = Array.from(tags.keys()).pop() || '';
+
+        core.info(`Generating release for ${packageName} ${packageVersion}`);
+
+        if (!releaseNotes) {
+            const commitSha: string = process.env.GITHUB_SHA || '';
+            const commitMessage: string = (await git(['log', '-1', '--pretty=%B', commitSha])).trim();
+            releaseNotes = commitMessage;
+        }
+
+        const releaseNotesLines = releaseNotes.split('\n');
+        const firstLineRegex = new RegExp(`^${packageName}\\s+v?${packageVersion}\\s+#(\\d+)$`);
+        let prNumber = '';
+        const firstLineMatch = releaseNotesLines[0].match(firstLineRegex);
+
+        if (firstLineMatch) {
+            prNumber = firstLineMatch[1];
+            releaseNotesLines.shift();
+            releaseNotes = releaseNotesLines.join('\n').trim();
+        }
+
+        let actor = '';
+
+        if (prNumber.length > 0) {
+            let pr: any | null = null;
+            try {
+                const octokit = github.getOctokit(githubToken, { required: true });
+                const { data } = await octokit.rest.pulls.get({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    pull_number: parseInt(prNumber)
+                });
+                pr = data;
+            } catch (error) {
+                core.warning(`Failed to get PR #${prNumber} details: ${error}`);
+            }
+            actor = pr?.user?.login || github.context.actor || process.env.GITHUB_ACTOR || '';
+        }
+
+        const prInfo = prNumber ? ` in #${prNumber}` : '';
+        let finalReleaseNotes = `## What's Changed\n- ${packageName} ${packageVersion} by @${actor}${prInfo}`;
+
+        if (releaseNotes.length > 0) {
+            finalReleaseNotes += `\n\n${releaseNotes.split('\n').map(line => `  ${line}`).join('\n')}`;
+        }
+
+        if (lastTag.length > 0) {
+            finalReleaseNotes += `\n\n**Full Changelog**: https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/compare/${lastTag}...${packageVersion}`;
+        } else {
+            finalReleaseNotes += `\n\n**Full Changelog**: https://github.com/${github.context.repo.owner}/${github.context.repo.repo}/commits/${packageVersion}`;
+        }
+
+        core.info(`Release Notes:\n${finalReleaseNotes}`);
+
+        // must use a unity editor 6000.3 or newer
+        const unityVersion = new UnityVersion('6000.3');
+        const unityHub = new UnityHub();
+        const unityEditor = await unityHub.GetEditor(unityVersion, undefined, ['f', 'b']);
+        const packageDir = path.dirname(packageJsonPath);
+        const outputDir = process.env.RUNNER_TEMP;
+
+        await unityEditor.Run({
+            args: [
+                '-batchmode',
+                '-username', username,
+                '-password', password,
+                '-cloudOrganization', organizationId,
+                '-upmPack', packageDir, outputDir,
+            ]
+        });
+
+        const tgzGlobber = await glob.create(path.join(outputDir, '*.tgz'));
+        const tgzFiles = await tgzGlobber.glob();
+
+        if (tgzFiles.length === 0) {
+            throw new Error('Signed .tgz file not found in the output directory');
+        }
+
+        const signedTgzPath = tgzFiles[0];
+
+        core.info(`Signed package created at ${signedTgzPath}`);
+
+        await git(['tag', '-a', packageVersion, '-m', `${packageName} ${packageVersion}`]);
+        await git(['push', 'origin', packageVersion]);
+        const octokit = github.getOctokit(githubToken, { required: true });
+        const { data: release } = await octokit.rest.repos.createRelease({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            tag_name: packageVersion,
+            name: `${packageName} ${packageVersion}`,
+            body: finalReleaseNotes,
+            generate_release_notes: false,
+            target_commitish: github.context.ref.replace('refs/heads/', ''),
+            draft: true
+        });
+
+        core.info(`Release created: ${release.html_url}`);
+
+        // upload release asset
+        const { data: asset } = await octokit.rest.repos.uploadReleaseAsset({
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            release_id: release.id,
+            name: path.basename(signedTgzPath),
+            data: signedTgzPath,
+            headers: {
+                'content-type': 'application/tar+gzip',
+                'content-length': (await fs.stat(signedTgzPath)).size
+            }
+        });
+
+        core.info(`Release asset uploaded: ${asset.browser_download_url}`);
     } catch (error) {
         core.setFailed(error);
     }
 }
 
 main();
+
+/**
+ * Get the list of valid SemVer tags and their parent sha
+ * @returns Map of tags and their parent sha
+ */
+async function getTags(): Promise<Map<string, string>> {
+    const semverRegex = /^v?\d+\.\d+\.\d+$/;
+    const tags = (await git(['tag', '--list', `--sort=version:refname`])).split('\n').filter(tag => tag.trim() !== '').filter(tag => semverRegex.test(tag));
+    const tagMap = new Map<string, string>();
+    for (let i = 0; i < tags.length; i++) {
+        const tag = tags[i];
+        tagMap.set(tag, await getParentSha(tag));
+    }
+    return tagMap;
+}
+
+/**
+ * Get the parent sha of a ref
+ * @param ref Ref to get parent sha for
+ * @returns Parent sha
+ */
+async function getParentSha(ref: string): Promise<string> {
+    return (await git(['rev-parse', '--verify', `${ref}^{}`])).trim();
+}
+
+/**
+ * Run a git command
+ * @param params Git command parameters
+ * @returns Git command output
+ */
+async function git(params: string[], warnOnError: boolean = true): Promise<string> {
+    let output: string = '';
+    let error: string = '';
+    const exitCode = await exec('git', params, {
+        listeners: {
+            stdout: (data: Buffer) => {
+                output += data.toString();
+            },
+            stderr: (data: Buffer) => {
+                error += data.toString();
+            }
+        }
+    });
+    if (exitCode !== 0) {
+        throw new Error(error);
+    }
+    if (error && warnOnError) {
+        core.warning(error);
+    }
+    return output;
+}
